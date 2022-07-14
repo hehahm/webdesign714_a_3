75e26a4d61404ab2b1781573657b3938b8cc6624
hehahm <hhahm@hotmail.com>
2022-07-14 13:13:49 +0900

    A-3 상단메뉴 추가 수정

--- js/base.js
+++ js/base.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-    
-})
+        $("#slideAni").animate({
+            "marginLeft": -1200*$(this).index() //태그자체가 갖고 있는 순번이 .index
+        },300, function(){})
+    })
