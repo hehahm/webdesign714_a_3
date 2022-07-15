c6e1fb2aa40beb42892f1dcc74b8e2c376f414d8
hehahm <hhahm@hotmail.com>
2022-07-15 14:28:02 +0900

    A-3 슬라이드 자바까지

--- js/base.js
+++ js/base.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-        $("#slideAni").animate({
-            "marginLeft": -1200*$(this).index() //태그자체가 갖고 있는 순번이 .index
-        },300, function(){})
-    })
+
+    //setInterval은 주기적으로 처리하는 명령, 화면 열리지마자는 불가/특정시간 흘러야 함 
+    //setTimeout은 특정시간이 흐르고 나서 딱 한 번! 실행
+
+    setInterval(function(){
+    //쌤왈 매운맛 버전 - 자식들이 야바위 게임하고 있다
+
+            $('.aniSlide').animate({
+                'marginLeft' : "-=1200"//그 값이 뭔지 모르겠지만 1200 마이너스해라
+            },400,function(){})
+                $('.aniSlide > div:first-child').appendTo($('.antiSlide'))
+                $('.antiSlide').css('margin-left',0)
+        }, 1000)
+        })
